dda5c4d250831a202ba7edaa2549b61c1bc2ff61
samet <sametcn99@gmail.com>
2023-09-09 02:27:05 +0300

    some changes

---
+++ src/app/footer.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Image from "next/image";
+
+const Footer = () => {
+  return (
+    <footer className="text-center py-4 fixed bottom-0 w-full">
+      <a
+        href="https://github.com/sametcn99/personal-website"
+        className="text-blue-500 text-xs font-sans hover:underline"
+        target="_blank"
+      >
+        Check out the Source Code from GitHub
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;

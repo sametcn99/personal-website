f2b82f6e38cc88954f19c01aebd4ff6a9cd82720
samet <sametcn99@gmail.com>
2023-09-09 01:49:31 +0300

    some changes

--- src/app/(social)/social/page.tsx
+++ src/app/(social)/social/page.tsx
@@ -10,23 +10,26 @@ export const metadata: Metadata = {
 
 export default function Social() {
   return (
-    <div className="h-screen flex items-center justify-center">
-      <div className="bg-gray-100 p-8 rounded-lg space-y-3">
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="p-8 space-y-3 rounded-2xl">
         <div className="flex items-center justify-center">
           <Image
             src={"/icon.png"}
             alt="Picture of the author"
-            width={150}
-            height={150}
+            width={120}
+            height={120}
           />
         </div>
-        <div className="text-center text-sm font-mono font-bold tracking-tight text-black  ">
-          you can reach me through these links
+        <div className="pb-5 text-center text-xs text-white font-mono font-bold tracking-tight">
+          You can reach me through these links
         </div>
-        <div className="space-y-2 mt-2 pt-5">
+        <div className="space-y-2 mt-4">
           {socialLinks.map((link, index) => (
             <Link legacyBehavior key={index} href={link.href}>
-              <a className="block w-full h-full bg-zinc-800 hover:bg-zinc-700 px-28 py-2 rounded-lg text-center text-xl text-white lowercase ">
+              <a
+                className="block w-full rounded-lg bg-zinc-800 sm:px-32 md:px-60 py-2 text-center text-white lowercase hover:scale-105 hover:bg-zinc-700"
+                target="_blank"
+              >
                 {link.name}
               </a>
             </Link>

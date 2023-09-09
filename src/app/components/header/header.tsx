4794238d85ba60a8c570dd4fa02f841a0a641cea
samet <sametcn99@gmail.com>
2023-09-10 02:22:47 +0300

    some chanes

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -17,7 +17,7 @@ export const Header = () => {
     // Create an array 'navLinks' containing navigation links as objects.
     { label: "Home", href: "/" },
     { label: "Social Links", href: "/social" },
-    { label: "Projects", href: "/projects" }, // Fixed a typo: "Pojects" -> "Projects".
+    { label: "Projects", href: "/projectspg" },
     { label: "Contact", href: "/contact" },
   ];
 

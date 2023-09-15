c98b617417b6d50bf19e7b233ee9e8c04a52b738
samet <sametcn99@gmail.com>
2023-09-15 04:21:24 +0300

    some changes

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,16 +3,8 @@ import Link from "next/link";
 // Define the NotFound component, which will be displayed for 404 (not found) pages.
 export default function NotFound() {
   return (
-    <>
-      <div className="text-xl text-white h-screen flex flex-col items-center justify-center">
-        I cannot find the page you are looking for :/
-        <a
-          href="/"
-          className="bg-gray-900 text-white uppercase select-none rounded-full p-4 mt-4 hover:bg-gray-700 hover:scale-105"
-        >
-          take me home
-        </a>
-      </div>
-    </>
+    <div className="text-white h-screen flex items-center justify-center select-none">
+      I cannot find the page you are looking for :/
+    </div>
   );
 }

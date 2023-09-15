5b7da55af949a477d6d2fac302b3c578095a4079
samet <sametcn99@gmail.com>
2023-09-15 05:08:00 +0300

    some changes

--- src/app/(pages)/social/page.tsx
+++ src/app/(pages)/social/page.tsx
@@ -9,45 +9,37 @@ import TelegramIcon from "@mui/icons-material/Telegram";
 export default function Social() {
   // Define a default export function named Social.
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      {/* Create a container div with minimum height to center content vertically */}
-      <div className="rounded-2xl">
-        {/* Create a container div with padding and spacing */}
-        <div className="flex items-center justify-center">
-          {/* Create a div to center content horizontally */}
-          <Image
-            unoptimized={true}
-            src={"/icon.png"}
-            alt="Picture of the author"
-            width={120}
-            height={120}
-            onDragStart={(e) => e.preventDefault()}
-            onContextMenu={(e) => e.preventDefault()}
-            onSelect={(e) => e.preventDefault()}
-          />
-          {/* Display an image with specified dimensions */}
-        </div>
-        {/* Display a text message */}
-        <div className="space-y-2 mt-4">
-          {/* Create a container for social links */}
+    <>
+      <div className="min-h-screen flex flex-col items-center justify-center space-y-3">
+        <Image
+          unoptimized={true}
+          src={"/icon.png"}
+          alt="Picture of the author"
+          width={120}
+          height={120}
+          onDragStart={(e) => e.preventDefault()}
+          onContextMenu={(e) => e.preventDefault()}
+          onSelect={(e) => e.preventDefault()}
+        />
+        <ul className="space-y-2">
           {socialLinks.map((link, index) => (
-            // Map through the socialLinks array and create a Link for each link.
-            <a
-              key={index}
-              className="block rounded-lg bg-zinc-800 px-36 py-2 text-center text-white lowercase hover:scale-105 hover:bg-zinc-700 select-none"
-              target="_blank"
-              href={link.href}
-            >
-              {link.name}
-            </a>
+            <li key={index}>
+              <a
+                className="block rounded-lg bg-zinc-800 px-20 py-1 text-center text-white lowercase hover:scale-105 hover:bg-zinc-700 select-none whitespace-nowrap"
+                target="_blank"
+                href={link.href}
+              >
+                {link.name}
+              </a>
+            </li>
           ))}
-        </div>
-        <div className="flex justify-center scale-110 mt-3">
+        </ul>
+        <div>
           <Tooltip title="Send me an email!">
             <IconButton
               aria-label="delete"
               size="large"
-              className="hover:bg-zinc-700 "
+              className="hover:bg-zinc-700"
               onClick={() =>
                 (window.location.href = "mailto:sametcn99@gmail.com")
               }
@@ -67,6 +59,6 @@ export default function Social() {
           </Tooltip>
         </div>
       </div>
-    </div>
+    </>
   );
 }
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,3 @@
-import Link from "next/link";
-
-// Define the NotFound component, which will be displayed for 404 (not found) pages.
 export default function NotFound() {
   return (
     <div className="text-white h-screen flex items-center justify-center select-none">

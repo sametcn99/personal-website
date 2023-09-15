c4aa7ba15165e66b237da09c8e747527e03757c6
samet <sametcn99@gmail.com>
2023-09-15 21:56:38 +0300

    some changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 "use client";
+import { IconButton } from "@mui/material";
 import About from "./(pages)/about/page";
 import Social from "./(pages)/social/page";
+import ArrowCircleDownIcon from "@mui/icons-material/ArrowCircleDown";
 
 export default function Home() {
   return (
-    <>
+    <div>
       <About />
       <Social />
-    </>
+    </div>
   );
 }

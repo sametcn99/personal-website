3c993866e642a2a3378503804edea2ba7ee2f4a9
samet <sametcn99@gmail.com>
2023-09-29 16:44:40 +0300

    sc

--- src/app/components/markdowncomponent/markdown.tsx
+++ src/app/components/markdowncomponent/markdown.tsx
@@ -7,24 +7,23 @@ import LinearProgress from "@mui/material/LinearProgress";
 export default function MarkdownCompoment() {
   const [markdownContent, setMarkdownContent] = useState<string>("");
   const [isLoading, setIsLoading] = useState<boolean>(true);
-
-  useEffect(() => {
-    async function fetchMarkdownContent() {
-      try {
-        const response = await fetch(
-          "https://raw.githubusercontent.com/sametcn99/personal-website/master/README.md"
-        );
-        if (response.ok) {
-          const markdownText: string = await response.text();
-          setMarkdownContent(markdownText);
-          setIsLoading(false);
-        } else {
-          console.error("Failed to retrieve Markdown content.");
-        }
-      } catch (error) {
-        console.error("Error pulling Markdown content:", error);
+  async function fetchMarkdownContent() {
+    try {
+      const response = await fetch(
+        "https://raw.githubusercontent.com/sametcn99/personal-website/master/README.md"
+      );
+      if (response.ok) {
+        const markdownText: string = await response.text();
+        setMarkdownContent(markdownText);
+        setIsLoading(false);
+      } else {
+        console.error("Failed to retrieve Markdown content.");
       }
+    } catch (error) {
+      console.error("Error pulling Markdown content:", error);
     }
+  }
+  useEffect(() => {
     fetchMarkdownContent();
   }, []);
   return isLoading ? (

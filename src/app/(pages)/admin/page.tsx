8a41f0e2415c2623dad136d7d166655891c0fc26
samet <sametcn99@gmail.com>
2023-10-04 20:17:44 +0300

    sc

--- src/app/(pages)/admin/page.tsx
+++ src/app/(pages)/admin/page.tsx
@@ -1,5 +1,35 @@
 "use client";
 
+import { QueryResultRow, sql } from "@vercel/postgres";
+import { useEffect, useState } from "react";
+
 export default function AdminPage() {
-  return <main></main>;
+  const [rows, setRows] = useState<QueryResultRow[] | null>(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const { rows } = await sql`SELECT * from social_links`;
+        setRows(rows); // Verileri rows state'ine atıyoruz
+      } catch (error) {
+        console.error("Verileri alırken bir hata oluştu:", error);
+      }
+    }
+
+    fetchData();
+  }, []);
+  if (rows === null) {
+    // Veriler henüz yüklenmediyse bir yükleniyor mesajı veya bileşeni gösterebilirsiniz
+    return <div>Loading...</div>;
+  }
+  return (
+    <main>
+      {/* Veritabanından gelen verileri kullanabilirsiniz */}
+      <ul>
+        {rows.map((row) => (
+          <li key={row.id}>{row.link}</li>
+        ))}
+      </ul>
+    </main>
+  );
 }

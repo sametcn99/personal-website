198a6fee15c1cdb8576ab47551708b0b50e78d60
samet <sametcn99@gmail.com>
2023-10-05 20:05:18 +0300

    sc

--- src/app/(pages)/admin/page.tsx
+++ src/app/(pages)/admin/page.tsx
@@ -1,11 +1,12 @@
-import { getSocialLinks } from "@/app/api/fetch-links";
+import { sql } from "@vercel/postgres";
+
 export default async function AdminPage() {
-  const rows = await getSocialLinks();
+  const { rows } = await sql`SELECT * from social_links`;
 
   return (
     <div className="flex flex-col items-center">
       <div className="flex flex-wrap justify-center mt-12">
-        {rows.map((row) => (
+        {rows.map((row:any) => (
           <div className="border p-4 m-4 text-center bg-gray-200" key={row.id}>
             <p className="font-semibold">ID: {row.id}</p>
             <p>Name: {row.name}</p>

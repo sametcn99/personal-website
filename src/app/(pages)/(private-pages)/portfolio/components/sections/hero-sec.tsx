70e4f99f738537b73f2953e1c6caa5b72c848850
samet <sametcn99@gmail.com>
2023-10-13 23:44:49 +0300

    added some animation to hero section

--- src/app/(pages)/(private-pages)/portfolio/components/sections/hero-sec.tsx
+++ src/app/(pages)/(private-pages)/portfolio/components/sections/hero-sec.tsx
@@ -1,19 +1,63 @@
+"use client";
+import Image from "next/image";
+import hello from "../../lib/hello.json";
+import { useEffect, useState } from "react";
+
 /**
  * Renders the hero section of the portfolio page.
  * @returns JSX element
  */
 export default function HeroSec() {
+  const [greeting, setGreeting] = useState("Hello World");
+  const greetingsArray = Object.values(hello.greetings);
+
+  useEffect(() => {
+    /**
+     * Sets a random greeting from an array of greetings every second using setInterval.
+     */
+    const interval = setInterval(() => {
+      const randomIndex = Math.floor(Math.random() * greetingsArray.length);
+      const randomGreeting = greetingsArray[randomIndex];
+      setGreeting(randomGreeting);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [greetingsArray]);
+
+  useEffect(() => {
+    /**
+     * Scrolls the window to a specific position and then back to the top after a delay.
+     * @param {number} delay - The delay in milliseconds before scrolling back to the top.
+     */
+    const timeout = setTimeout(() => {
+      window.scrollTo(0, 100);
+      setTimeout(() => {
+        window.scrollTo(0, 0);
+      }, 1000);
+    }, 1000);
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
     <div
-      className="h-screen w-screen flex flex-col justify-center items-center"
+      className="h-screen w-screen flex flex-col justify-center items-center select-none"
       id="hero-sec"
     >
-      <span className="text-white text-center space-y-2">
+      <Image
+        src={"blobanimation.svg"}
+        alt="Picture of the author"
+        width={500}
+        height={500}
+        loading="lazy"
+        className="absolute flex justify-center items-center"
+      />
+      <span className="text-white text-center space-y-2 z-10">
         <h1 className="text-5xl font-bold">
-          Hello!
-          <br /> I&apos;m a Full-Stack Software Developer
+          <span>{greeting}</span>
+          <br />
+          <span>I&apos;m a Full-Stack Software Developer</span>
+          <br />
+          <span className="text-3xl">Welcome to my portfolio!</span>
         </h1>
-        <h2 className="text-3xl">Welcome to my portfolio!</h2>
       </span>
     </div>
   );

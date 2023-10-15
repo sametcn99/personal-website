312c874d3d9cfd6d22b241e033ef5a6b5a604dc5
samet <sametcn99@gmail.com>
2023-10-15 17:45:29 +0300

    sc

--- src/app/(pages)/(public-pages)/social/components/links.tsx
+++ src/app/(pages)/(public-pages)/social/components/links.tsx
@@ -1,4 +1,7 @@
 import { sql } from "@vercel/postgres";
+import "../../../../styles/globals.css";
+import applyDelayedAnimation from "@/app/styles/list-item-delay";
+
 
 export default async function LinkTable() {
   /**
@@ -9,9 +12,10 @@ export default async function LinkTable() {
   return (
     <ul className="space-y-4">
       {rows.map((link, index) => (
-        <li key={index}>
+        <li key={index} style={applyDelayedAnimation(index)}>
           <a
             className="
+            list-item-animation 
             block rounded-lg px-20 md:px-32 py-1
             outline outline-slate-500 outline-offset-1 
             text-center md:text-xl text-white lowercase select-none whitespace-nowrap

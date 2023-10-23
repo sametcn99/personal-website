7c155d24d1a8dacf4392bdee2616418e01cf88aa
sametcn99 <sametcn99@gmail.com>
2023-10-24 02:27:43 +0300

    sc

--- src/app/components/header/lib/page-links.ts
+++ src/app/components/header/lib/page-links.ts
@@ -1,6 +1,4 @@
 export const initialButtons = [
   { id: 1, text: "Home", href: "/", active: false },
-  { id: 2, text: "Social", href: "/social", active: false },
   { id: 3, text: "Blog", href: "/blog", active: false },
-  { id: 4, text: "About", href: "/about", active: false },
 ];
--- src/app/styles/lib/list-item-delay.ts
+++
@@ -1,6 +0,0 @@
-export default function applyDelayedAnimation(index: number, delay: number) {
-  return {
-    animation: `listItemFadeIn 1s ease forwards`,
-    animationDelay: `${index * delay}s`,
-  };
-}

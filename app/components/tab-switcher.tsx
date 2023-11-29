48cf8f291f30c2659fa70a0e792768c3c1295cae
sametcn99 <sametcn99@gmail.com>
2023-11-29 04:14:59 +0300

    sc

--- app/components/tab-switcher.tsx
+++ app/components/tab-switcher.tsx
@@ -12,20 +12,7 @@ export default function TabSwitcher() {
           defaultSelectedKey={"projects"}
           aria-label="Options"
           className={`w-full flex items-center justify-center z-50 p-2 sticky top-0 `}
-          //onChange={handleTabChange}
         >
-          <Tab key="blog" title="Blog">
-            <Card className="z-10 gap-11 space-y-4 w-full text-sm text-white bg-opacity-50 select-none hover:scale-105 max-w-[35rem]">
-              <CardBody className="w-full text-center break-words text-wrap">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
-                sit amet sagittis quam, vel sollicitudin urna. Mauris ultricies
-                sapien consectetur consectetur aliquam. Duis maximus in velit
-                vitae efficitur. Nunc in diam ut nulla tempor tristique ut eu
-                ipsum. Vivamus in risus ullamcorper, sodales lorem nec, lobortis
-                leo. Donec placerat accumsan aliquam.
-              </CardBody>
-            </Card>
-          </Tab>
           <Tab key="projects" title="Projects">
             <Projects />
           </Tab>

d32eefa3322bcef0ca0cd848da82314bfebf4013
sametcn99 <sametcn99@gmail.com>
2023-12-20 01:39:46 +0300

    code cleanup

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,7 +1,12 @@
 export default function Loading() {
   return (
-    <div className="flex h-full w-full animate-pulse items-center justify-center">
-      <div className="h-12 w-12 animate-spin rounded-full border-8 border-t-4 border-dashed light:border-black dark:border-white"></div>
+    <div
+      className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+      role="status"
+    >
+      <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+        Loading...
+      </span>
     </div>
   );
 }

788afabb99986c160b32bb22ba299f0f4934a6fc
sametcn99 <sametcn99@gmail.com>
2024-01-04 03:34:14 +0300

    seo optimization

---
+++ app/(pages)/about/layout.tsx
@@ -0,0 +1,19 @@
+import { Metadata } from "next";
+import React from "react";
+
+const meta = {
+  title: "About Me | Samet Can Cıncık",
+  description: "About Me | Samet Can Cıncık",
+};
+
+export const metadata: Metadata = {
+  title: meta.title,
+  description: meta.description,
+  openGraph: {
+    title: meta.title,
+    description: meta.description,
+  },
+};
+export default function layout({ children }: any) {
+  return <div>{children}</div>;
+}

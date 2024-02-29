10c8d47ef6069efbcabca1728eb478db9a50d850
samet <sametcn99@gmail.com>
2024-02-29 22:42:51 +0300

    redirect to resume

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -4,6 +4,8 @@ import { GitHubRepo } from "@/types";
 import { fetchRepo } from "@/utils/utils";
 import { redirect } from "next/navigation";
 
+const resumeSlugs = ["cv", "resume", "ozgecmis"];
+
 export async function generateMetadata({
   params,
 }: {
@@ -21,6 +23,16 @@ export async function generateMetadata({
         openGraph: {
           title: `${label} | Samet Can Cıncık`,
           description: `My ${label} Profile`,
+        },
+      };
+    }
+    if (resumeSlugs.includes(params.slug)) {
+      return {
+        title: `${params.slug} | Samet Can Cıncık`,
+        description: `My ${params.slug}`,
+        openGraph: {
+          title: `${params.slug} | Samet Can Cıncık`,
+          description: `My ${params.slug}`,
         },
       };
     }
@@ -55,6 +67,13 @@ export default async function Page({ params }: { params: { slug: string } }) {
     if (socialMediaLink) {
       redirect(`/redirect?url=${socialMediaLink.link}`);
       return null; // Ensure to return null after redirect
+    }
+
+    if (resumeSlugs.includes(params.slug)) {
+      redirect(
+        "/redirect?url=https://docs.google.com/document/d/1lYhA_7M2-g0JzlqlZlDibM_bksowBTuSqkrVF-7moKs/edit?usp=sharing",
+      );
+      return null;
     }
 
     const repo = await fetchRepo(params.slug);

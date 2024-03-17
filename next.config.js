3740ce9682748fa083521c8a1000d99310c43d0d
samet <sametcn99@gmail.com>
2024-03-17 21:24:10 +0300

    add gpv link and leetcode link

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   /* config options here */
   productionBrowserSourceMaps: true,
+  images: {
+    domains: ['raw.githubusercontent.com', 'githubprofileviewer.com'],
+  }
 };
 
 module.exports = nextConfig;

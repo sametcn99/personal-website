8a7f9c4751bfd5105f0a27e533cf755d15db435b
samet <sametcn99@gmail.com>
2024-08-14 22:49:00 +0300

    feat: Add PDF download button with icon
    This commit adds a PDF download button to the DownloadButton component. The button now includes an icon from the react-icons library, specifically the FaRegFilePdf icon. The button's styling has also been updated to include a border, a backdrop blur effect, and a hover effect. The text "Kaydet" has been replaced with the icon and the text "Kaydet" for smaller screens.

--- src/components/DownloadButton.tsx
+++ src/components/DownloadButton.tsx
@@ -1,4 +1,6 @@
 'use client'
+import { FaRegFilePdf } from 'react-icons/fa6'
+
 export default function DownloadButton() {
 	const handleDownload = () => {
 		window.print()
@@ -7,9 +9,13 @@ export default function DownloadButton() {
 	return (
 		<button
 			onClick={handleDownload}
-			className='fixed bottom-4 right-4 rounded-xl bg-purple-950 p-3 text-white transition-all duration-700 hover:bg-black/30 dark:hover:bg-white/30 print:hidden'
+			className='fixed bottom-4 right-4 inline-flex gap-2 rounded-xl border border-white/30 bg-purple-950/40 p-3 text-white backdrop-blur-sm transition-all duration-700 hover:bg-black/10 dark:hover:bg-white/10 print:hidden'
 		>
-			Kaydet
+			<FaRegFilePdf
+				className='inline'
+				size={24}
+			/>
+			<span className='hidden font-semibold md:block'>Kaydet</span>
 		</button>
 	)
 }

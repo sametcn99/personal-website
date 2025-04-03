7ea3fdf1caebc9d5517a74dea49d47b1641dfab6
sametcn99 <sametcn99@gmail.com>
2025-04-03 14:10:19 +0300

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -133,7 +133,7 @@ export default function RootLayout({
 										<SidebarHeader className='px-2 py-4'>
 											<div className='mb-2'>
 												<Link
-													href='/gist'
+													href='/'
 													className='flex items-center gap-2 text-sm font-medium'
 												>
 													<HomeIcon className='h-4 w-4' />

66162193c3568bd3b0cba036c3cec43eb7cc512b
sametcn99 <sametcn99@gmail.com>
2025-04-06 21:18:52 +0300

    .

--- scripts/generate-sidebar.ts
+++ scripts/generate-sidebar.ts
@@ -1,54 +1,56 @@
-import * as fs from 'fs';
-import * as path from 'path';
+import * as path from 'path'
+import * as fs from 'fs'
 
 interface SidebarLink {
-  title: string;
-  href: string;
+	title: string
+	href: string
 }
 
 interface SidebarData {
-  Personal: SidebarLink[];
-  Gists: SidebarLink[];
+	Personal: SidebarLink[]
+	Gists: SidebarLink[]
 }
 
-const gistDir = path.join(__dirname, '../src/app/gist');
+const gistDir = path.join(__dirname, '../src/app/gist')
 
 function generateSidebarLinks(dir: string): void {
-  const items = fs.readdirSync(dir, { withFileTypes: true });
-  
-  const links: SidebarLink[] = items
-    .filter(item => item.isDirectory())
-    .map(folder => {
-      const title = folder.name
-        .split('-')
-        .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-        .join(' ');
-      
-      return {
-        title,
-        href: `/gist/${folder.name}`
-      };
-    })
-    .sort((a, b) => a.title.localeCompare(b.title));
+	const items = fs.readdirSync(dir, { withFileTypes: true })
 
-  const sidebarData: SidebarData = {
-    Personal: [
-      { title: 'Home', href: '/' },
-      { title: 'CV', href: '/cv' }
-    ],
-    Gists: links
-  };
+	const links: SidebarLink[] = items
+		.filter((item) => item.isDirectory())
+		.map((folder) => {
+			const title = folder.name
+				.split('-')
+				.map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+				.join(' ')
 
-  // Create data directory if it doesn't exist
-  const dataDir = path.join(__dirname, '../src/data');
-  if (!fs.existsSync(dataDir)) {
-    fs.mkdirSync(dataDir, { recursive: true });
-  }
+			return {
+				title,
+				href: `/gist/${folder.name}`,
+			}
+		})
+		.sort((a, b) => a.title.localeCompare(b.title))
 
-  fs.writeFileSync(
-    path.join(__dirname, '../src/data/sidebar-links.json'),
-    JSON.stringify(sidebarData, null, 2)
-  );
+	const sidebarData: SidebarData = {
+		Personal: [
+			{ title: 'Home', href: '/' },
+			{ title: ' Blog', href: '/blog' },
+			{ title: 'CV', href: '/cv' },
+			{ title: 'Gists', href: '/gist' },
+		],
+		Gists: links,
+	}
+
+	// Create data directory if it doesn't exist
+	const dataDir = path.join(__dirname, '../src/data')
+	if (!fs.existsSync(dataDir)) {
+		fs.mkdirSync(dataDir, { recursive: true })
+	}
+
+	fs.writeFileSync(
+		path.join(__dirname, '../src/data/sidebar-links.json'),
+		JSON.stringify(sidebarData, null, 2)
+	)
 }
 
-generateSidebarLinks(gistDir);
+generateSidebarLinks(gistDir)

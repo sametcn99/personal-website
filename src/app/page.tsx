0af7828c59cde8712b4a4aa48a8e0e92e5218b92
sametcn99 <sametcn99@gmail.com>
2025-05-01 19:46:31 +0300

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,17 +16,9 @@ import Divider from "@mui/material/Divider";
 import HomeIcon from "@mui/icons-material/Home";
 import Description from "@mui/icons-material/Description";
 import Code from "@mui/icons-material/Code";
-import SearchIcon from "@mui/icons-material/Search";
-import IconButton from "@mui/material/IconButton";
-import Tooltip from "@mui/material/Tooltip";
-import { SearchModal } from "@/components/SearchModal";
-import { useSearchShortcuts } from "@/lib/useSearchShortcuts";
-import Chip from "@mui/material/Chip";
-import KeyboardIcon from "@mui/icons-material/Keyboard";
 
 export default function Home() {
 	const visibleLinks = socialMediaLinks.filter((link) => link.visible);
-	const [searchOpen, setSearchOpen] = React.useState(false);
 
 	// Function to render an icon based on the title
 	const getIconForItem = (title: string) => {
@@ -35,19 +27,6 @@ export default function Home() {
 		return <Code />; // Default icon for gists
 	};
 
-	// Open search modal
-	const handleOpenSearch = () => {
-		setSearchOpen(true);
-	};
-
-	// Close search modal
-	const handleCloseSearch = () => {
-		setSearchOpen(false);
-	};
-	
-	// Setup keyboard shortcuts
-	useSearchShortcuts({ openSearch: handleOpenSearch });
-
 	return (
 		<Box
 			display="flex"
@@ -55,31 +34,7 @@ export default function Home() {
 			alignItems="center"
 			minHeight="100vh" // Ensure the box takes full viewport height
 		>
-			<Container maxWidth="sm">				{/* Search Button */}
-				<Box display="flex" justifyContent="flex-end" alignItems="center" mb={2}>
-					<Tooltip title="Press Ctrl+K or / to search">
-						<Chip 
-							icon={<KeyboardIcon />}
-							label="Ctrl+K"
-							size="small"
-							color="primary"
-							variant="outlined"
-							onClick={handleOpenSearch}
-							sx={{ mr: 1, cursor: 'pointer' }}
-						/>
-					</Tooltip>
-					<Tooltip title="Search Gists">
-						<IconButton 
-							color="primary" 
-							onClick={handleOpenSearch}
-							aria-label="search"
-							size="large"
-						>
-							<SearchIcon />
-						</IconButton>
-					</Tooltip>
-				</Box>
-
+			<Container maxWidth="sm">
 				{/* Social Media Links */}
 				<Typography variant="h6" gutterBottom>
 					Social Media
@@ -116,9 +71,6 @@ export default function Home() {
 						</Fade>
 					))}
 				</List>
-
-				{/* Search Modal */}
-				<SearchModal open={searchOpen} onClose={handleCloseSearch} />
 			</Container>
 		</Box>
 	);

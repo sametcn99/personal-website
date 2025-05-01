b64dac6760c5de8cb36de8f6c811712889bbcc8c
sametcn99 <sametcn99@gmail.com>
2025-05-01 18:06:08 +0300

    .

--- src/theme.ts
+++ src/theme.ts
@@ -1,8 +1,37 @@
-"use client";
-import { createTheme } from "@mui/material/styles";
+'use client';
+import { createTheme } from '@mui/material/styles';
+import { Roboto } from 'next/font/google';
+
+const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+});
 
 const theme = createTheme({
-	colorSchemes: { dark: true },
+  colorSchemes: { light: true, dark: true },
+  cssVariables: {
+    colorSchemeSelector: 'class',
+  },
+  typography: {
+    fontFamily: roboto.style.fontFamily,
+  },
+  components: {
+    MuiAlert: {
+      styleOverrides: {
+        root: {
+          variants: [
+            {
+              props: { severity: 'info' },
+              style: {
+                backgroundColor: '#60a5fa',
+              },
+            },
+          ],
+        },
+      },
+    },
+  },
 });
 
 export default theme;

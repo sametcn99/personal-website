18911a322abe787541ba806365203e4612cafb24
sametcn99 <sametcn99@gmail.com>
2025-05-19 03:04:57 +0300

    .

--- next.config.ts
+++ next.config.ts
@@ -1,27 +1,18 @@
 import createMDX from "@next/mdx";
 import rehypeHighlight from "rehype-highlight";
-import remarkFrontmatter from "remark-frontmatter";
 import remarkGfm from "remark-gfm";
-import remarkMdxFrontmatter from "remark-mdx-frontmatter";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Allow .mdx extensions for files
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
-  transpilePackages: [
-    "estree-walker",
-    "rehype-highlight",
-    "remark-frontmatter",
-    "remark-gfm",
-    "remark-mdx-frontmatter",
-  ],
   // Optionally, add any other Next.js config below
 };
 
 const withMDX = createMDX({
   // Add markdown plugins here, as desired
   options: {
-    remarkPlugins: [remarkGfm, remarkFrontmatter, remarkMdxFrontmatter],
+    remarkPlugins: [remarkGfm],
     rehypePlugins: [rehypeHighlight],
   },
 });

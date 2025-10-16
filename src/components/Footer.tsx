07d647ce398fdeb8d4a8e5da26b1a2d6b5968534
sametcn99 <sametcn99@gmail.com>
2025-10-16 04:18:59 +0300

    .

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,20 +2,27 @@
 
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import Link from "next/link";
 
 export default function Footer() {
   return (
     <Box textAlign="center" mt={4} justifyItems="center">
       <Box
         display="flex"
+        flexDirection={"column"}
         justifyContent="center"
         alignItems="center"
         gap={1}
         mb={1}
       >
-        <Typography variant="body2" color="text.secondary">
+        <Typography variant="caption" color="text.secondary">
           Last updated: {new Date().toLocaleDateString("tr-TR")}
         </Typography>
+        <Link href="/privacy-policy" target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none' }}>
+          <Typography variant="caption" color="text.secondary" sx={{ textDecoration: 'none', '&:hover': { textDecoration: 'underline' } }}>
+            Privacy Policy
+          </Typography>
+        </Link>
       </Box>
     </Box>
   );
